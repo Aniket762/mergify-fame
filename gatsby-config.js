4db2b614e7767750d09d327c8b7d6757b0005f4c
sumana2001 <sumana.basu2001@gmail.com>
2022-10-07 14:30:57 +0530

    feat: pwa added

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,10 +10,37 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-json`,
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Mergify Mastery`,
+        short_name: `Mergify`,
+        description: `Mergify Workshop on Merge Queues Automation`,
+        start_url: `/`,
+        background_color: `#06101B`,
+        theme_color: `#06101B`,
+        display: `standalone`,
+        icon: `static/images/mergify-logo.png`,
+        icons: [
+          {
+            src: `static/images/mergify-logo-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+            purpose: `maskable`
+          },
+          {
+            src: `static/images/mergify-logo.png`,
+            sizes: `512x512`,
+            type: `image/png`
+          }
+        ]
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `./src/data/`,
-      },
+      }
     },
+    `gatsby-plugin-offline`,
   ],
 }
